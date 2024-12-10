aad2db2eb35c8fbc851cbb0b275ae97d513f9c3c
Carolina Castro <144167622+carolCase@users.noreply.github.com>
2024-12-10 09:58:14 +0100

    launch app

---
+++ src/main/kotlin/com/carolCase/userdetails_service_password_encoders/UserdetailsServicePasswordEncodersApplication.kt
@@ -0,0 +1,11 @@
+package com.carolCase.userdetails_service_password_encoders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserdetailsServicePasswordEncodersApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserdetailsServicePasswordEncodersApplication>(*args)
+}
---
+++ src/test/kotlin/com/carolCase/userdetails_service_password_encoders/UserdetailsServicePasswordEncodersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.carolCase.userdetails_service_password_encoders
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserdetailsServicePasswordEncodersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
